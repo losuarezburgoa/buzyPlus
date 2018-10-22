5497547071de90a8047442007968ac1a135db781
losuarezburgoa <ludgerito@gmail.com>
2018-10-22 12:52:26 -0500

    Copyright message was put at the end of the file

--- discontClustToolBox/plot2dudata.m
+++ discontClustToolBox/plot2dudata.m
@@ -1,27 +1,3 @@
-## Copyright (C) 2018 Ludger O. Suarez-Burgoa
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {Function File} {@var{retval} =} plot2dudata (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: Ludger O. Suarez-Burgoa <ludger@ludger-Inspiron-3458>
-## Created: 2018-06-03
-
 function [pltfig] = plot2dudata(knNormEigVecCell, lCell, symbolSize, kSymbCell)
 
 K = length(knNormEigVecCell);
@@ -79,3 +55,27 @@ axis equal
 hold off
 
 endfunction
+
+## Copyright (C) 2018 Ludger O. Suarez-Burgoa
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {Function File} {@var{retval} =} plot2dudata (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Ludger O. Suarez-Burgoa <ludger@ludger-Inspiron-3458>
+## Created: 2018-06-03
